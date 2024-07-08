80a982673e90887af28c474cda25c4e6498c45d4
Lucien Rey <lucien.rey@swissit-design.com>
2024-07-08 21:45:31 +0200

    static added to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
