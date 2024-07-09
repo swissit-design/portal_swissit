b94d7bd19f736aeebc0e1c59aa262d3959b2951a
Lucien Rey <lucien.rey@swissit-design.com>
2024-07-09 07:42:49 +0200

    add post build

---
+++ backend/post_build.sh
@@ -0,0 +1,3 @@
+# Add manually the migration function when deploying migration as well
+# This needs to be added manually to Azure Web APPS as new variable -> POST_BUILD_SCRIPT_PATH
+python manage.py migrate
