d8c626c7d6d57e76261468adbf22471aeeb54321
Lucien Rey <lucien.rey@swissit-design.com>
2024-07-09 09:06:59 +0200

    adding requirement .txt with poetry

--- backend/post_build.sh
+++ backend/post_build.sh
@@ -1,3 +1,4 @@
 # Add manually the migration function when deploying migration as well
 # This needs to be added manually to Azure Web APPS as new variable -> POST_BUILD_SCRIPT_PATH
+# adding also this cmd linke to Startup Settings to install poetry
 python manage.py migrate
