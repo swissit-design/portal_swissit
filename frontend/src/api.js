0895dc90e1a553888b26e99800948bccd64a71da
Lucien Rey <lucien.rey@swissit-design.com>
2024-07-30 16:49:21 +0200

    new env variables react

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const isDevelopment = import.meta.env.MODE === 'development'
-const baseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
+// const isDevelopment = import.meta.env.MODE === 'development'
+// const baseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
+const baseURL = import.meta.env.REACT_APP_API_URL
 const api = axios.create({baseURL});
 
 api.interceptors.request.use(
